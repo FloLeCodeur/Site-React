bbf5d5a03a8e494f6ebfc22fc90b6e4cf4709886
Florian <mre.florian@gmail.com>
2022-10-09 16:40:27 +0200

    update Nav and create news

--- src/Components/Navbar/NavBar.js
+++ src/Components/Navbar/NavBar.js
@@ -29,25 +29,27 @@ export default function NavBar() {
         x
       </span>
       <nav>
-        <div className="site-name">
-          <Link to='/'>SneakersLife</Link>
-        </div>
-        <div className={menuToggle ? "nav-items-container active" : "nav-items-container"}>
-          <Link to='/Nouveaute'>Nouveautées</Link>
-          <Link to='/products'>Produits</Link>
-          <Link to='/contact'>Contact</Link>
-        </div>
-        <div className="right-items-nav">
-          <Link to='/cart'><span className="material-symbols-outlined">shopping_cart</span></Link>
-          <Link to='/register' className="login-inscription">
-          <span className="material-symbols-outlined">person</span>
-          </Link>
-        </div>
-        <div className="btn-nav-toggle">
-          <div onClick={handleMenuToggle} className="btn-menu">
-            <span className={menuToggle ?"top-line line active" : "top-line line"}></span>
-            <span className={menuToggle ?"center-line line active" : "center-line line"}></span>
-            <span className={menuToggle ?"bottom-line line active" : "bottom-line line"}></span>
+        <div className="navbar">
+          <div className="site-name">
+            <Link to='/'>SneakersLife</Link>
+          </div>
+          <div className={menuToggle ? "nav-items-container active" : "nav-items-container"}>
+            <Link to='/Nouveaute'>Nouveautées</Link>
+            <Link to='/products'>Produits</Link>
+            <Link to='/contact'>Contact</Link>
+          </div>
+          <div className="right-items-nav">
+            <Link to='/cart'><span className="material-symbols-outlined">shopping_cart</span></Link>
+            <Link to='/register' className="login-inscription">
+            <span className="material-symbols-outlined">person</span>
+            </Link>
+          </div>
+          <div className="btn-nav-toggle">
+            <div onClick={handleMenuToggle} className="btn-menu">
+              <span className={menuToggle ?"top-line line active" : "top-line line"}></span>
+              <span className={menuToggle ?"center-line line active" : "center-line line"}></span>
+              <span className={menuToggle ?"bottom-line line active" : "bottom-line line"}></span>
+            </div>
           </div>
         </div>
       </nav>
--- src/Pages/News/News.js
+++ src/Pages/News/News.js
@@ -3,11 +3,49 @@ import './News.css'
 
 export default function News() {
   return (
-    <div className='main-news'>
-      <div>
-        <div className='o'></div>
-        <div className="k"></div>
+    <main className="main-news">
+        <h1>Nouveautées</h1>
+      <div className='news-container'>
+        <div>
+          <div className='img-container'>
+            <img src={process.env.PUBLIC_URL + `ProductsImages/News/donald-martinez-EGWyfNEz6Ho-unsplash.jpg`} alt="" />
+          </div>
+          <div className="descrition-container">
+            <div className="description-wrapper">
+              <h2>Jordan 1 electro Orange</h2>
+              <span>Un coloris parfait pour l'automne et la chasse aux bonbons.</span>
+              <span>Un coloris noir et blanc ponctué de pièce d'un orange vif.</span>
+              <span className='btn-disconnect'>Bientôt disponible</span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className='img-container'>
+            <img src={process.env.PUBLIC_URL + `ProductsImages/News/junior-samson-BjhKc5QBzlg-unsplash.jpg`} alt="" />
+          </div>
+          <div className="descrition-container">
+            <div className="description-wrapper">
+              <h2>Jordan 1 Union Los Angeles Black Toe</h2>
+              <span>Tout droit sortie de la collaboration entre le shop américain et la marque à la virgule.</span>
+              <span>La jordan 1 Union Black Toe et l'une des paires les plus recherchée sur le marché.</span>
+              <span className='btn-disconnect'>Bientôt disponible</span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className='img-container'>
+            <img src={process.env.PUBLIC_URL + `ProductsImages/News/sacai-nike-vaporwaffle-black-white-cv1363-001-pic6.jpg`} alt="" />
+          </div>
+          <div className="descrition-container">
+            <div className="description-wrapper">
+              <h2>Nike VaporWaffle Sacai Black and White </h2>
+              <span>Un ovni de la sneakers tout droit sortit des ateliers de Chitose Abe.</span>
+              <span>La nike VaporWaffle Sacai est un mélange de Nike VaporFly et Pegasus.</span>
+              <span className='btn-disconnect'>Bientôt disponible</span>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </main>
   )
 }
